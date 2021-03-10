3eed6058627d95c8fbabb97bef23ce7d4dd7b429
darkomarjanovic89 <sportcoachdarko@gmail.com>
2021-03-10 23:11:45 +0100

    dodata navigacija, sa css-om

--- nav.php
+++ nav.php
@@ -1,12 +1,60 @@
 
+<!--nav login i registracija-->
 
-<!-- moze da bude i .html -->
-<nav>
-    <a href="index.php">Home</a>
-    <a href="shop.php">Shop</a>
-    <a href="admin/index.php">Admin</a>
-    <a href="cart.php">Cart</a>
-    <a href="register.php">Register</a>
-    <a href="login.php">Login</a>
-    <a href="inter/index.php">Inter</a>
+
+<nav class="navbar navbar-expand-lg">
+  <a class="navbar-brand" href="#">WebShop</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+
+  <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Shop</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>      
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-user-alt"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">login</a>
+          <a class="dropdown-item" href="#">register</a>
+         
+        </div>
+      </li>   
+    </ul>
+  </div>
 </nav>
+
+
+
+
+
+<!--jumbotron-->
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Welcome to web shop</h1>
+    <!-- <img src="Slike/3.jpeg" class="img-thumbnail " alt="..."> -->
+    <p class="lead">Welcome to webShop...</p>
+  </div>
+</div>
+
+<!--nav meni-->
+
+
+
+
+<!--slider-->
+
+
