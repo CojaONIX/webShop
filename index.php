f1c94c7c3b60bd9f588943023ce4bd73c181a6ea
milandoshljak <milandoshljak@gmail.com>
2021-03-13 15:53:33 +0100

    Add head to index.php

--- index.php
+++ index.php
@@ -1,37 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>phpShop - Home</title>
+<?php
+    include "views/components/head.php";
+    include "views/components/nav.php";
+?>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+<h1>Something about WEBSHOP...</h1>
+<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, quibusdam.</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, autem sapiente quisquam minima, quaerat iste nesciunt nemo fugit labore, ipsam maxime doloremque a eligendi. Laborum fugit amet est totam officia?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, autem sapiente quisquam minima, quaerat iste nesciunt nemo fugit labore, ipsam maxime doloremque a eligendi. Laborum fugit amet est totam officia?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, autem sapiente quisquam minima, quaerat iste nesciunt nemo fugit labore, ipsam maxime doloremque a eligendi. Laborum fugit amet est totam officia?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, autem sapiente quisquam minima, quaerat iste nesciunt nemo fugit labore, ipsam maxime doloremque a eligendi. Laborum fugit amet est totam officia?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, autem sapiente quisquam minima, quaerat iste nesciunt nemo fugit labore, ipsam maxime doloremque a eligendi. Laborum fugit amet est totam officia?</p>
 
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/main.js"></script>
-
-    <style>
-        
-    </style>
-</head>
-<body>
-    <?php
-        include "views/components/nav.php";
-    ?>
-
-    <h1>Home stranica</h1>
-
-    <?php
-        include "views/components/footer.php";
-    ?>
-
-    <script>
-    
-    </script>
+<?php
+    include "views/components/footer.php";
+?>
 </body>
 </html>
