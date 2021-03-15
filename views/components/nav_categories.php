aa335e73042cd6fc7e8da038a01f9c359725922e
CojaONIX <cojaonix@gmail.com>
2021-03-15 13:44:17 +0100

    Dinamicka navigacija katogorija iz baze

--- views/components/nav_categories.php
+++ views/components/nav_categories.php
@@ -1,41 +1,35 @@
 
 <hr>
-    <div id="catLeftMenu">
-        <h5>Categories</h5>
-        <ul>
-            <li>
-                <h5>Computers</h5>
-                <ul>
-                    <li><a href="shop.php?cat=1">Desktops</a></li>
-                    <li><a href="shop.php?cat=2">Notebooks</a></li>
-                    <li><a href="shop.php?cat=3">Software</a></li>
-                </ul>
-            </li>
-            <li>
-                <h5>Electronics</h5>
-                <ul>
-                    <li><a href="shop.php?cat=4">Camera &amp; photo</a></li>
-                    <li><a href="shop.php?cat=5">Cell phones</a></li>
-                    <li><a href="shop.php?cat=6">Others</a></li>
-                </ul>
-            </li>
-            <li>
-                <h5>Apparel</h5>
-                <ul>
-                    <li><a href="shop.php?cat=7">Shoes</a></li>
-                    <li><a href="shop.php?cat=8">Clothing</a></li>
-                    <li><a href="shop.php?cat=9">Accessories</a></li>
-                </ul>
-            </li>
-            <li>
-                <h5>Other</h5>
-                <ul>
-                    <li><a href="shop.php?cat=10">Digital Downloads</a></li>
-                    <li><a href="shop.php?cat=11">Books</a></li>
-                    <li><a href="shop.php?cat=12">Jewelry</a></li>
-                    <li><a href="shop.php?cat=13">Gift Cards</a></li>
-                </ul>
-            </li>
-        </ul>
-    </div>
+<h5>Categories</h5>
+<hr>
+
+<?php
+    require_once "../db/conn.php";
+    $query = "SELECT id, name
+                FROM main_categories;
+              SELECT id, name, main_categories_id
+                FROM categories;";
+
+    $conn->multi_query($query);
+    $mc_result = $conn->store_result();
+    $conn->next_result();
+    $c_result = $conn->store_result();
+
+    echo "<ul>";
+    foreach($mc_result as $row) {
+        echo "<li>";
+            echo "<h5>{$row['name']}</h5>";
+            echo "<ul>";
+            foreach($c_result as $row2) {
+                if($row2['main_categories_id'] == $row['id']) {
+                    echo "<li><a href='shop.php?cat=" . $row2['id'] . "'>" . $row2['name'] . "</a></li>";
+                }
+            }
+            echo "</ul>";
+        echo "</li>";
+    }
+    echo "</ul>";
+
+?>
+
 <hr>
