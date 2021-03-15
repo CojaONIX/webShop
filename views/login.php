57549df0c716e8c6e1864f0c1667f5f0b45844e5
milandoshljak <milandoshljak@gmail.com>
2021-03-15 19:03:59 +0100

    Change login/register forms style

--- views/login.php
+++ views/login.php
@@ -1,42 +1,37 @@
-
 <?php
-    require_once "components/head.php";
-?>
-    
-<?php
-    require_once './components/nav.php';
+  require_once "components/head.php";
+  require_once './components/nav.php';
 ?>
 
-<div class="container">
-    <div class="row mt-6">
-        <div class="card col-12 col-sm-8 col-md-6 col-lg-4 mx-auto">
-            <div class="card-body">
-                <form>
-                    <div class="mb-3">
-                        <label for="username" class="form-label">Username</label>
-                        <input
-                        type="text"
-                        class="form-control"
-                        id="username"
-                        required
-                        />
-                    </div>
-                    <div class="mb-4">
-                        <label for="password" class="form-label">Password</label>
-                        <input
-                        type="password"
-                        class="form-control"
-                        id="password"
-                        required
-                        />
-                    </div>
-                    <div class="d-grid">
-                        <button class="btn btn-primary" type="submit">LOGIN</button>
-                    </div>
-                </form>
+  <div class="container">
+    <div class="row px-2">
+      <div class="card col-12 col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card-body">
+          <form>
+            <div class="mb-3">
+              <label for="username" class="form-label">Username</label>
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                required
+              />
             </div>
+            <div>
+              <label for="password" class="form-label">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="password"
+                required
+              />
+            </div>
+            <small class="d-block my-3">Doesn't have an account? <a href="register.php">Register now</a></small>
+            <button class="btn btn-primary btn-block mb-1" type="submit">LOGIN</button>
+          </form>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </body>
 </html>
--- views/register.php
+++ views/register.php
@@ -1,114 +1,113 @@
-
 <?php
-    require_once "components/head.php";
-?>
-    
-<?php
-    require_once './components/nav.php';
+  require_once "components/head.php";
+  require_once './components/nav.php';
 ?>
 
-<div class="container">
-    <div class="row mt-6">
-        <div class="card col-12 col-sm-8 col-md-6 col-lg-4 mx-auto">
-            <div class="card-body">
-                <form>
-                    <div class="row">
-                        <div class="mb-3 col">
-                        <label for="firstName" class="form-label">First name</label>
-                        <input
-                            type="text"
-                            class="form-control"
-                            id="firstName"
-                            required
-                        />
-                        </div>
-                        <div class="mb-3 col">
-                        <label for="lastName" class="form-label">Last name</label>
-                        <input
-                            type="text"
-                            class="form-control"
-                            id="lastName"
-                            required
-                        />
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="mb-3 col">
-                        <label for="country" class="form-label">Country</label>
-                        <input
-                            type="text"
-                            class="form-control"
-                            id="country"
-                            required
-                        />
-                        </div>
-                        <div class="mb-3 col">
-                        <label for="city" class="form-label">City</label>
-                        <input type="text" class="form-control" id="city" required />
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="mb-3 col">
-                        <label for="postalCode" class="form-label">Postal code</label>
-                        <input
-                            type="text"
-                            class="form-control"
-                            id="postalCode"
-                            required
-                        />
-                        </div>
-                        <div class="mb-3 col">
-                        <label for="phone" class="form-label">Phone</label>
-                        <input type="text" class="form-control" id="phone" required />
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="address" class="form-label">Address</label>
-                        <input type="text" class="form-control" id="address" required />
-                    </div>
-
-                    <hr />
-
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email" required />
-                    </div>
-                    <div class="mb-3">
-                        <label for="username" class="form-label">Username</label>
-                        <input
-                        type="text"
-                        class="form-control"
-                        id="username"
-                        required
-                        />
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input
-                        type="password"
-                        class="form-control"
-                        id="password"
-                        required
-                        />
-                    </div>
-                    <div class="mb-4">
-                        <label for="confirmPassword" class="form-label"
-                        >Confirm password</label
-                        >
-                        <input
-                        type="password"
-                        class="form-control"
-                        id="confirmPassword"
-                        required
-                        />
-                    </div>
-                    <div class="d-grid gap-2">
-                        <button class="btn btn-primary" type="submit">LOGIN</button>
-                    </div>
-                </form>
+  <div class="container">
+    <div class="row px-2">
+      <div class="card col-12 col-sm-11 col-md-9 col-lg-7 mx-auto">
+        <div class="card-body">
+          <form>
+            <div class="row">
+              <div class="mb-3 col-12 col-sm-6">
+                <label for="firstName" class="form-label">First name</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="firstName"
+                  required
+                />
+              </div>
+              <div class="mb-3 col-12 col-sm-6">
+                <label for="lastName" class="form-label">Last name</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="lastName"
+                  required
+                />
+              </div>
             </div>
+            <div class="row">
+              <div class="mb-3 col-12 col-sm-6">
+                <label for="country" class="form-label">Country</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="country"
+                  required
+                />
+              </div>
+              <div class="mb-3 col-12 col-sm-6">
+                <label for="city" class="form-label">City</label>
+                <input type="text" class="form-control" id="city" required />
+              </div>
+            </div>
+            <div class="row">
+              <div class="mb-3 col">
+                <label for="postalCode" class="form-label">Postal code</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="postalCode"
+                  required
+                />
+              </div>
+              <div class="mb-3 col">
+                <label for="phone" class="form-label">Phone</label>
+                <input type="text" class="form-control" id="phone" required />
+              </div>
+            </div>
+            <div class="mb-4">
+              <label for="address" class="form-label">Address</label>
+              <input type="text" class="form-control" id="address" required />
+            </div>
+
+            <hr style="background-color: #ccc; padding: 1px; border: none">
+
+            <div class="row">
+              <div class="mb-3 col-12 col-sm-6">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" required />
+              </div>
+              <div class="mb-3 col-12 col-sm-6">
+                <label for="username" class="form-label">Username</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="username"
+                  required
+                />
+              </div>
+            </div>
+            <div class="row">
+              <div class="mb-3 col-12 col-sm-6">
+                <label for="password" class="form-label">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password"
+                  required
+                />
+              </div>
+              <div class="mb-3 col-12 col-sm-6">
+                <label for="confirmPassword" class="form-label"
+                  >Confirm password</label
+                >
+                <input
+                  type="password"
+                  class="form-control"
+                  id="confirmPassword"
+                  required
+                />
+              </div>
+            </div>
+            <small class="d-block mb-3">Already have an account? <a href="login.php">Log in</a></small>
+            <button class="btn btn-primary btn-block mb-1" type="submit">REGISTER</button>
+          </form>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </body>
 </html>
