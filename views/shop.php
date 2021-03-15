cc338560fcb4dff9e631c45bab35d8408b852736
CojaONIX <cojaonix@gmail.com>
2021-03-15 11:22:49 +0100

    Sve slike za proizvode

--- views/shop.php
+++ views/shop.php
@@ -16,43 +16,41 @@
     </div>
 
     <div id="productsList" class="col-md-8 col-lg-9">
-        <div class="container">
-            <div class="row">
-                <?php
-                    $cat_id = isset($_GET['cat']) ? $_GET['cat'] : 1;
-                    require_once "../db/conn.php";
-                    $sql = "SELECT * FROM products WHERE categories_id = $cat_id;";            
-                    if ($result = $conn->query($sql)) {
-                        foreach($result as $row) {
-                            ?>
+        <div class="row">
+            <?php
+                $cat_id = isset($_GET['cat']) ? $_GET['cat'] : 1;
+                require_once "../db/conn.php";
+                $sql = "SELECT * FROM products WHERE categories_id = $cat_id;";            
+                if ($result = $conn->query($sql)) {
+                    foreach($result as $row) {
+                        ?>
 
-                                <div class="col-sm-6 col-lg-4 card-group px-2 mb-3">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <h5 class="card-title"><?php echo $row['name']; ?></h5>
-                                        </div>
+                            <div class="col-sm-6 col-lg-4 card-group px-2 mb-3">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5 class="card-title"><?php echo $row['name']; ?></h5>
+                                    </div>
 
-                                        <img class="card-img-top" src="images/products/<?php echo $row['id']; ?>.jpeg" onerror="this.onerror=null; this.src='images/noImage.jpg'">
+                                    <img class="card-img-top" src="images/products/<?php echo $row['id']; ?>.jpeg" onerror="this.onerror=null; this.src='images/noImage.jpg'">
 
-                                        <div class="card-body">
-                                            <p><?php echo $row['short_description']; ?></p>
-                                        </div>
+                                    <div class="card-body">
+                                        <p><?php echo $row['short_description']; ?></p>
+                                    </div>
 
-                                        <div class="card-footer">
-                                            <h3><?php echo $row['price']; ?></h3>
-                                            <a class="btn btn-primary stretched-link" href="shop.php">Shop</a>
-                                        </div>
+                                    <div class="card-footer">
+                                        <h3><?php echo $row['price']; ?></h3>
+                                        <a class="btn btn-primary stretched-link" href="shop.php">Shop</a>
                                     </div>
                                 </div>
+                            </div>
 
-                            <?php
-                        }
-
-                    } else {
-                        echo "<p>Error: " . $sql . " --- " . $conn->error . "</p>";
+                        <?php
                     }
-                ?>
-            </div>
+
+                } else {
+                    echo "<p>Error: " . $sql . " --- " . $conn->error . "</p>";
+                }
+            ?>
         </div>
     </div>
 </div>
