542f55816085cf71bcc9874e473dda16a6d6605d
milandoshljak <milandoshljak@gmail.com>
2021-03-15 15:13:19 +0100

    Modify navbar

--- views/components/nav.php
+++ views/components/nav.php
@@ -1,43 +1,44 @@
+<!-- TODO: ovde treba da se ispita da li je korisnik logovan, ako nije prikazi samo login/register -->
+<!-- TODO: ako jeste, prikazi, ostalo i logout... -->
+<!-- TODO: ako je korisnik admin (role === "admin" - treba da se prepravi baza) onda ubaci i link ka admin stranicama (ADMIN) -->
+<!-- TODO: mozda da se stavi podmeni za admina ako ima vise linkova/stranica, ili na admin stranicama da se ubaci jos jedan navbar -->
 
-<style>
+<!-- REMINDER: neka u tabeli "users" default "role" vrednost php postavlja kao "user", a rucno ce se unositi admini (role === "admin")  -->
 
-</style>
+<nav class="navbar navbar-expand-md navbar-light bg-light mb-5">
 
-<div class="container">
-  <nav class="navbar navbar-expand-md navbar-light bg-light ms-3 me-2 mb-5">
-    <a id="logo" class="navbar-brand" href="home.php">webShop</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  <a id="logo" class="navbar-brand" href="home.php">webShop</a>
 
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-      <ul class="navbar-nav flex-grow-1">
-        <li class="nav-item active">
-          <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="shop.php">Shop</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
 
-      <ul class="navbar-nav nav text-center">
-        <li class="nav-item">
-          <a class="nav-link" href="login.php">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="register.php">Register</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-</div>
+    <ul class="navbar-nav flex-grow-1 text-center">
+      <li class="nav-item active">
+        <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="shop.php">Shop</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
 
+    <ul class="navbar-nav nav text-center">
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link pr-0" href="register.php">Register</a>
+      </li>
+    </ul>
 
+  </div>
 
+</nav>
