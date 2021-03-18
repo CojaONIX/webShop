5f42f6a94b9ca0d1b188cc8e16a1d4f1992b2e93
CojaONIX <cojaonix@gmail.com>
2021-03-18 20:37:03 +0100

    Checkout login provera

--- views/checkout.php
+++ views/checkout.php
@@ -17,7 +17,7 @@
     <hr>
 
     <?php
-        if(!isset($_SESSION["id"])) { 
+        if(isset($_SESSION["user_id"])) { 
             ?>
             <div class="container">
                 <table id="cartData" class="table table-sm">
@@ -27,6 +27,35 @@
                     </tr>
                 </table>
             </div>
+
+            <script>
+                jsonCheckout = JSON.parse(localStorage.getItem("jsonCart"));
+                var subTotal = total = 0;
+                $.each( jsonCheckout, function(index, value) {
+                    subTotal = value['pprice'] * value['pqty'];
+                    total += subTotal;
+                    $('#cartData').append(`
+                        <tr>
+                            <td rowspan=2><img src="images/products/${value['pid']}.jpeg"></td>
+                            <td colspan=3>${value['pname']}</td>
+                            <tr>
+                                <td>${value['pprice']}</td>
+                                <td class="text-center">${value['pqty']}</td>
+                                <td class="text-right">${subTotal.toFixed(2)}</td>
+                            </tr>
+                        <tr>
+                    `);
+                });
+
+                $('#cartData').append(`
+                    <tr>
+                        <th colspan=2></th>
+                        <th class="text-right">Total:</th>
+                        <th class="text-right">${total.toFixed(2)}</th>
+                    <tr>
+                `);
+            </script>
+
             <?php
         } else {
             echo "<h4 class='text-center'>To continue shopping, please login</h4>";
@@ -36,34 +65,6 @@
     <button class="btn btn-info" onclick="window.history.back();">Cancel</button>
 
 
-    <script>
-        jsonCheckout = JSON.parse(localStorage.getItem("jsonCart"));
-        var subTotal = total = 0;
-        $.each( jsonCheckout, function(index, value) {
-            subTotal = value['pprice'] * value['pqty'];
-            total += subTotal;
-            $('#cartData').append(`
-                <tr>
-                    <td rowspan=2><img src="images/products/${value['pid']}.jpeg"></td>
-                    <td colspan=3>${value['pname']}</td>
-                    <tr>
-                        <td>${value['pprice']}</td>
-                        <td class="text-center">${value['pqty']}</td>
-                        <td class="text-right">${subTotal.toFixed(2)}</td>
-                    </tr>
-                <tr>
-            `);
-        });
 
-        $('#cartData').append(`
-            <tr>
-                <th colspan=2></th>
-                <th class="text-right">Total:</th>
-                <th class="text-right">${total.toFixed(2)}</th>
-            <tr>
-        `);
-
- 
-    </script>
 </body>
 </html>
--- views/components/commercials.php
+++ views/components/commercials.php
@@ -1,7 +1,8 @@
 
 <div class="container">
     <hr>
-    <h2 class="text-center">webShop - Welcome to our store</h2>
+    <h4 class="text-center">webShop</h4>
+    <h2 class="text-center">Welcome to our store</h2>
     <hr>
 
     <h5>Online shopping is the process consumers go through to purchase products or services over the Internet. You can edit this in the <a href="../admin">admin site</a>.</h5>
